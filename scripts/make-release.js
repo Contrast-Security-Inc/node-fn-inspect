a2bd1f3f5edc722022a5036ffa09488540dc6ccc
jcolekaplan <jacob.kaplan@contrastsecurity.com>
2021-03-10 15:51:36 -0500

    Download latest artifact, bump version and publish it, bump local checkout

--- scripts/make-release.js
+++ scripts/make-release.js
@@ -2,33 +2,22 @@
 'use strict';
 const yargsInteractive = require('yargs-interactive');
 const cp = require('child_process');
-const os = require('os');
 const fs = require('fs');
-const pa = require('path');
-
 const options = {
   interactive: { default: true },
   releaseType: {
     type: 'list',
     describe: 'What kind of release do you want to make?',
     choices: ['patch', 'minor', 'major']
-  },
-  funcinfoPath: {
-    type: 'input',
-    describe: 'What directory is funcinfo.tgz.zip in?'
   }
 };
 
 yargsInteractive()
   .usage('$0 [args]')
   .interactive(options)
-  .then(({ releaseType, funcinfoPath }) => {
-    funcinfoPath =
-      funcinfoPath[0] === '~' ? funcinfoPath.substring(2) : funcinfoPath;
-    const path = pa.resolve(os.homedir(), funcinfoPath);
-
-    if (fs.existsSync(`${path}/funcinfo.tgz.zip`)) {
-      cp.execSync(`${__dirname}/npm-publish.sh ${releaseType} ${path}`, {
+  .then(({ releaseType }) => {
+    if (fs.existsSync('funcinfo.tgz.zip')) {
+      cp.execSync(`${__dirname}/npm-publish.sh ${releaseType}`, {
         stdio: 'inherit'
       });
     } else {
@@ -43,4 +32,6 @@ yargsInteractive()
   .catch((err) => {
     console.log('Publishing failed:');
     console.log(err);
+    process.exit(-1);
   });
+  
--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash -e
 
-if [[ -z $1 || -z $2 ]]; then
-  echo "Please specify semver type and directory containing funcinfo.tgz.zip"
+if [[ -z $1 ]]; then
+  echo "Please specify semver type"
   exit 255
 fi
 
 SEMVER_TYPE=$1
-FUNCINFO_PATH=$2
 
 #Clear any old funcinfo files
-rm -rf $FUNCINFO_PATH/funcinfo
-rm -rf $FUNCINFO_PATH/funcinfo.tgz
+rm -rf funcinfo
+rm -rf funcinfo.tgz
 
-unzip $FUNCINFO_PATH/funcinfo.tgz.zip -d $FUNCINFO_PATH
-mkdir $FUNCINFO_PATH/funcinfo
-tar -xzvf $FUNCINFO_PATH/funcinfo.tgz -C $FUNCINFO_PATH/funcinfo
+unzip funcinfo.tgz.zip
+mkdir funcinfo
+tar -xzvf funcinfo.tgz -C funcinfo
 
 echo "Publishing module"
-npm version $SEMVER_TYPE --prefix $FUNCINFO_PATH/funcinfo/package
-npm publish $FUNCINFO_PATH/funcinfo/package
+npm version $SEMVER_TYPE --prefix funcinfo/package
+npm publish funcinfo/package
