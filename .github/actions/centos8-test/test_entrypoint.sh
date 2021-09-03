b6dfb816cebc13e4d2ad598bd65a812e14354c0e
jcolekaplan <jacob.kaplan@contrastsecurity.com>
2021-09-03 13:13:41 -0400

    Centos8, bump node-gyp, python3, chmod +x

--- .github/actions/centos8-test/test_entrypoint.sh
+++ .github/actions/centos8-test/test_entrypoint.sh
@@ -3,7 +3,9 @@
 NODE_VERSION=$1
 curl -sL https://rpm.nodesource.com/setup_$NODE_VERSION.x | bash -
 
+yum install -y python3
 yum install -y nodejs make gcc-c++ valgrind
 
 npm install --unsafe-perm
+chmod +x test/test.js
 npm run test:valgrind
