e61ef09895cfd5e7540ff664bc43c80b0c70498f
Griffin Yourick <gryphon92@gmail.com>
2022-06-09 14:25:19 -0400

    Update LICENSE, README, add download script

---
+++ scripts/download-artifacts.js
@@ -0,0 +1,116 @@
+/* eslint-disable no-process-exit */
+// @ts-check
+'use strict';
+
+const { Octokit } = require('@octokit/rest');
+const https = require('https');
+const path = require('path');
+const rimraf = require('rimraf');
+const unzipper = require('unzipper');
+
+const { AUTH_TOKEN } = process.env;
+
+if (!AUTH_TOKEN) {
+  console.error(
+    'ERROR: You must set AUTH_TOKEN to a GitHub personal access token with the `repo` scope and SSO Authorization.'
+  );
+  console.error(
+    'See: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token'
+  );
+  process.exit(1);
+}
+
+const octokit = new Octokit({ auth: AUTH_TOKEN });
+const owner = 'Contrast-Security-Inc';
+const repo = 'node-fn-inspect';
+
+const prebuildsDir = path.resolve(__dirname, '../prebuilds');
+
+// calculate 90 days ago
+const date = new Date(Date.now() - 90 * 24 * 60 * 60 * 1000);
+
+// find most recent runs
+octokit.actions
+  .listWorkflowRunsForRepo({
+    owner,
+    repo,
+    status: 'success',
+    created: `>=${date.toISOString()}`,
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  })
+  .then(({ data }) => {
+    // we are only interested in "Release" runs
+    const runs = data.workflow_runs.filter((run) => run.name === 'Release');
+
+    if (runs.length < 1) {
+      console.error(
+        'ERROR: Unable to find any release workflow runs within the last 90 days'
+      );
+      process.exit(1);
+    }
+
+    const { id } = runs.reduce((max, run) =>
+      max.run_number > run.run_number ? max : run
+    );
+
+    // retrieve artifact data from most recent workflow run within last 90 days
+    return octokit.actions.listWorkflowRunArtifacts({
+      owner,
+      repo,
+      run_id: id,
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  })
+  .then(({ data }) => {
+    if (data.artifacts.length < 1) {
+      console.error('ERROR: Unable to find any artifacts for the provided run');
+      process.exit(1);
+    }
+
+    // clean up any existing prebuilds
+    return new Promise((resolve, reject) => {
+      rimraf(prebuildsDir, (err) => {
+        if (err) reject(err);
+        resolve();
+      });
+    }).then(() => data.artifacts);
+  })
+  .then((artifacts) =>
+    // download and extract each artifact to prebuilds/<artifact>
+    Promise.all(
+      artifacts.map(async (artifact) => {
+        const { url } = await octokit.actions.downloadArtifact({
+          owner,
+          repo,
+          artifact_id: artifact.id,
+          archive_format: 'zip',
+        });
+
+        const artifactDir = path.resolve(prebuildsDir, artifact.name);
+        const stream = unzipper.Extract({ path: artifactDir });
+
+        return new Promise((resolve) => {
+          https.get(url, (res) => {
+            res.pipe(stream).on('close', () => {
+              console.log(
+                'artifact %s downloaded and extracted',
+                artifact.name
+              );
+              resolve();
+            });
+          });
+        });
+      })
+    )
+  )
+  .catch((err) => {
+    console.error('ERROR: Unable to download and extract artifacts');
+    console.error(err);
+    process.exit(1);
+  });
