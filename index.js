bb5723d23e8a686cb7f18dafd4b6f70f27d381f0
Griffin Yourick <gryphon92@gmail.com>
2022-06-09 14:53:41 -0400

    Tweak CODE_EVENT_TYPES

--- index.js
+++ index.js
@@ -2,26 +2,9 @@
 
 const binding = require('node-gyp-build')(__dirname);
 
-/**
- * ```
- * V(BUILTIN_TAG, Builtin)
- * V(CALLBACK_TAG, Callback)
- * V(EVAL_TAG, Eval)
- * V(FUNCTION_TAG, Function)
- * V(INTERPRETED_FUNCTION_TAG, InterpretedFunction)
- * V(HANDLER_TAG, Handler)
- * V(BYTECODE_HANDLER_TAG, BytecodeHandler)
- * V(LAZY_COMPILE_TAG, LazyCompile)
- * V(REG_EXP_TAG, RegExp)
- * V(SCRIPT_TAG, Script)
- * V(STUB_TAG, Stub)
- * V(NATIVE_FUNCTION_TAG, Function)
- * V(NATIVE_LAZY_COMPILE_TAG, LazyCompile)
- * V(NATIVE_SCRIPT_TAG, Script)
- * ```
- */
+// From v8-profiler.h
 const CODE_EVENT_TYPES = [
-  '', // 0 unused
+  '', // unknown
   'BUILTIN',
   'CALLBACK',
   'EVAL',
@@ -33,9 +16,7 @@ const CODE_EVENT_TYPES = [
   'REG_EXP',
   'SCRIPT',
   'STUB',
-  'NATIVE_FUNCTION',
-  'NATIVE_LAZY_COMPILE',
-  'NATIVE_SCRIPT',
+  'RELOCATION',
 ];
 
 let codeEventsInited = false;
