364c33f73d4cde8118415c95abfed14213ae9a41
Griffin Yourick <gryphon92@gmail.com>
2022-06-09 14:56:41 -0400

    Cleanup

--- src/func-info.cc
+++ src/func-info.cc
@@ -9,10 +9,7 @@ NAN_METHOD(FuncInfo) {
     Local<Value> resourceName = fn->GetScriptOrigin().ResourceName();
 
     if (!resourceName.IsEmpty()) {
-        // file
         Nan::Set(obj, Nan::New<String>("file").ToLocalChecked(), resourceName);
-
-        // lineNumber
         Nan::Set(obj,
                  Nan::New<String>("lineNumber").ToLocalChecked(),
                  Nan::New<Integer>(fn->GetScriptLineNumber()));
